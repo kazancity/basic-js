ddf73d0382b8764a33a11adabf55d78ed7d71c1d
Artem Banderov <artem_75@bk.ru>
2022-05-01 14:16:01 +0300

    Delete dns-stats.js

--- src/dns-stats.js
+++
@@ -1,2 +0,0 @@
-module.exports.getDNSStats=(domains)=>{obj={};domains.forEach(d=>{b=d.split('.').reverse();
-b.forEach((s,i)=>{ar=b.slice(0,i);c='.'+ar.concat(b[i]).join('.');if(obj[c]){obj[c]++}else obj[c]=1})});return obj}
