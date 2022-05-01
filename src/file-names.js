c1380f80d82ead24e55fcaeb1114f95d2839b352
kazancity <kazancity@aol.com>
2022-05-01 20:27:24 +0300

    init: start file-names task

--- src/file-names.js
+++ src/file-names.js
@@ -1,2 +1,2 @@
-module.exports.renameFiles=(names)=>{z=Array(0),a={};for(b of names){
+exports.renameFiles=(names)=>{z=Array(0),a={};for(b of names){
 if(b in a){x=`${b}(${a[b]})`;z.push(x);a[x]=1;a[b]++}else{z.push(b);a[b]=1}}return z}
