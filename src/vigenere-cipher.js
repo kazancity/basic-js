63ee5e41411ff09b512d4880bb9a6755dcda6f52
kazancity <kazancity@aol.com>
2022-05-02 16:37:27 +0300

    init: start vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,3 +3,4 @@ exports.VigenereCipheringMachine=class VigenereCipheringMachine{
   if(!msg||!i){throw Error("Incorrect arguments!")}[msg,i]=[msg,i].map((str)=>str.toUpperCase());for(b of msg){c=b.charCodeAt(0);
   if(c>=65&&c<=90){ltrKey=(i[a++%i.length].charCodeAt(0)-65)%32;ltrChar=decrypt?c+65-ltrKey:c-65+ltrKey;b=String.fromCharCode(ltrChar%26+65)}
   arr.push(b)}return(this.direct?arr:arr.reverse()).join("")}decrypt(msg,i){return this.encrypt(msg,i,1)}}
+  
