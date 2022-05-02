8cc563428248b26e9cf1272081796490cf14b6b2
Artem Banderov <artem_75@bk.ru>
2022-05-02 13:15:57 +0300

    Delete simple-chain.js

--- src/simple-chain.js
+++
@@ -1,3 +0,0 @@
-module.exports.chainMaker={a:[],getLength(){return this.a.length},addLink(z=''){this.a.push(`( ${z} )`);return this},
-removeLink(y){i=--y;if(!Number.isInteger(i)||i<0||i>=this.a.length){this.a=Array(0);throw new Error("You can't remove incorrect link!")}
-this.a.splice(i,1);return this},reverseChain(){this.a.reverse();return this},finishChain(){res=this.a.join('~~');this.a=Array(0);return res}}
