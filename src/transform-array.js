902f72637e393dd87eff883a98317d34f4a8274b
kazancity <kazancity@aol.com>
2022-05-02 15:04:27 +0300

    init: start transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
-
-module.exports = {
-  transform
-};
+exports.transform=arr=>{if(!Array.isArray(arr))throw new Error('\'arr\' parameter must be an instance of the Array!')
+z=arr.reduce((a,b,c)=>{if(arr[c-1]=='--discard-next')return a
+if(arr[c-1]=='--double-next')a.push(arr[c]);if(arr[c+1]=='--double-prev')a.push(arr[c]);if(arr[c+1]=='--discard-prev')return a
+if(b=='--discard-next'||b=='--double-prev'||b=='--double-next'||b=='--discard-prev')return a;a.push(arr[c]);return a},Array(0));return z}
