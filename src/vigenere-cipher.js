6194360865f6ccbda78591482c3fcb526155d5cf
kazancity <kazancity@aol.com>
2022-05-02 16:34:10 +0300

    init: start vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
-
-module.exports = {
-  VigenereCipheringMachine
-};
+exports.VigenereCipheringMachine=class VigenereCipheringMachine{
+  constructor(direct=true){this.direct=direct}encrypt(msg,i,decrypt=0){var a=0,b,c,ltrKey,ltrChar,arr=Array(0);
+  if(!msg||!i){throw Error("Incorrect arguments!")}[msg,i]=[msg,i].map((str)=>str.toUpperCase());for(b of msg){c=b.charCodeAt(0);
+  if(c>=65&&c<=90){ltrKey=(i[a++%i.length].charCodeAt(0)-65)%32;ltrChar=decrypt?c+65-ltrKey:c-65+ltrKey;b=String.fromCharCode(ltrChar%26+65)}
+  arr.push(b)}return(this.direct?arr:arr.reverse()).join("")}decrypt(msg,i){return this.encrypt(msg,i,1)}}
